9a5a9bb75a09413b24624707b772c38112ebe3af
_rika <i@ksqsf.moe>
2019-04-10 16:59:51 +0800

    Select correct major modes for .v files

--- modules/prelude-prog.el
+++ modules/prelude-prog.el
@@ -74,4 +74,32 @@ mode."
   (pop-to-buffer (make-comint "node-repl" "node" nil "--interactive")))
 
 
+;;; Resolve Coq and Verilog
+(defvar coq-or-verilog-mode--regexp "\\(?:\\(Theorem\\|Ltac\\|Example\\|Lemma\\)[ 	]\\).+:")
+(defun coq-or-verilog-mode ()
+  "Analyze buffer and enable either Coq or Verilog mode.
+
+Coq uses .v extension for Coq files, which is also the one used
+for Verilog files.  This makes matching on file name insufficient
+for detecting major mode that should be used.
+
+This function attempts to use file contents to determine whether
+the code is Coq or Verilog and based on that chooses whether to
+enable `coq-mode' or `verilog-mode'.
+
+The testing procedure and criteria are not sufficiently enough,
+but works well enough for distinguishing source files in Software
+Foundations and typical Verilog files."
+  (if (save-excursion
+        (save-restriction
+          (save-match-data
+            (widen)
+            (goto-char (point-min))
+            (re-search-forward coq-or-verilog-mode--regexp
+                               (point-max) t))))
+      (coq-mode)
+    (verilog-mode)))
+(add-to-list 'auto-mode-alist '("\\.v\\'" . coq-or-verilog-mode))
+
+
 (provide 'prelude-prog)
