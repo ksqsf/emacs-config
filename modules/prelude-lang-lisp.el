d43045b172ade136e605a3eae733ff70e52723a4
_rika <i@ksqsf.moe>
2019-04-28 09:57:43 +0900

    Prefer Guile as the default Scheme implementation

--- modules/prelude-lang-lisp.el
+++ modules/prelude-lang-lisp.el
@@ -8,4 +8,11 @@
 (add-hook 'lisp-mode-hook #'prelude--enter-lisp)
 (add-hook 'emacs-lisp-mode-hook #'prelude--enter-lisp)
 
+;; Scheme prefers Guile
+(eval-after-load 'scheme
+  (setq scheme-program-name "guile"))
+
+(eval-after-load 'guile
+  (setq geiser-default-implementation 'guile))
+
 (provide 'prelude-lang-lisp)
