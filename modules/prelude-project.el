3f2ce15536933db15348d0d505ecd78f84badbf0
ksqsf <i@ksqsf.moe>
2019-07-29 01:04:36 +0800

    Use C-c p as the prefix for projectile commands
    M-p is useful for history navigation

--- modules/prelude-project.el
+++ modules/prelude-project.el
@@ -4,7 +4,7 @@
 (ensure-package 'counsel-projectile)
 
 (require 'projectile)
-(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
 (counsel-projectile-mode +1)
 
