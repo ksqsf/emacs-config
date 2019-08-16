954360c0e8cb4f690ce2fcfe94fbd9b554b5b767
ksqsf <i@ksqsf.moe>
2019-08-17 05:47:11 +0800

    [eshell] send notification when command is done and eshell buffer is buried

--- modules/prelude-other.el
+++ modules/prelude-other.el
@@ -3,4 +3,34 @@
 (ensure-package 'wakatime-mode)
 (global-wakatime-mode t)
 
+;; eshell sends notifications when commands finished
+;; stolen from https://blog.hoetzel.info/post/eshell-notifications/
+(ensure-package 'alert)
+(require 'alert)
+
+(when (fboundp 'mac-do-applescript)
+  (defun alert-osx-notifier-notify (info)
+    "Patched to make use of Emacs MacPort's native AppleScript
+  capability."
+    (mac-do-applescript (format "display notification %S with title %S"
+                                (alert-encode-string (plist-get info :message))
+                                (alert-encode-string (plist-get info :title))))
+    (alert-message-notify info)))
+
+(defun eshell-command-alert (process status)
+  (let* ((cmd (process-command process))
+          (buffer (process-buffer process))
+          (msg (format "%s: %s" (mapconcat 'identity cmd " ") status)))
+    (if (string-prefix-p "finished" status)
+        (alert msg :buffer buffer :severity 'normal)
+      (alert msg :buffer buffer :severity 'urgent))))
+
+(alert-add-rule :status '(buried)
+                :mode 'eshell-mode
+                :style (if (string-equal system-type "darwin")
+                           'osx-notifier
+                         'notifications))
+
+(add-hook 'eshell-kill-hook #'eshell-command-alert)
+
 (provide 'prelude-other)
