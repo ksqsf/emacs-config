64c68a57737c0e94557a038cbfbc9e59d4ea2703
ksqsf <i@ksqsf.moe>
2019-08-26 12:00:54 +0800

    Patch `fixup-whitespace` to stop `join-line` insert whitespace between CJK chars

--- modules/prelude-chinese.el
+++ modules/prelude-chinese.el
@@ -14,4 +14,18 @@
 ;; spell checking.
 (setq ispell-dictionary "en_US")
 
+;; Overwrite the builtin version, to stop `join-line' (alias of
+;; `delete-indentation') insert whitespace between CJK characters.
+(defun fixup-whitespace ()
+  "Fixup whitespace between objects around point.
+Leave one space or none, according to the context."
+  (interactive "*")
+  (save-excursion
+    (delete-horizontal-space)
+    (if (or (looking-at "^\\|$\\|\\s)\\|\\cc\\|\\cj\\|\\ck")
+	    (save-excursion (forward-char -1)
+			    (looking-at "$\\|\\s(\\|\\s'\\|\\cc\\|\\cj\\|\\ck")))
+	nil
+      (insert ?\s))))
+
 (provide 'prelude-chinese)
