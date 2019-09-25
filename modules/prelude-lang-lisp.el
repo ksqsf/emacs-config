246f1c0be2f9e78e0982ea158d16ec67894cb39e
ksqsf <i@ksqsf.moe>
2019-09-25 19:03:40 +0800

    SICM

--- modules/prelude-lang-lisp.el
+++ modules/prelude-lang-lisp.el
@@ -5,11 +5,14 @@
   :config
   (add-hook 'lisp-interaction-mode-hook #'(lambda () (define-key paredit-mode-map "\C-j" nil))))
 
-;; Scheme prefers Guile
 (eval-after-load 'scheme
-  (setq scheme-program-name "guile"))
+  (setq scheme-program-name "mechanics"))
 
 (eval-after-load 'guile
   (setq geiser-default-implementation 'guile))
+
+(defun mechanics ()
+  (interactive)
+  (run-scheme "/usr/local/scmutils/mit-scheme/bin/scheme --library /usr/local/scmutils/mit-scheme/lib"))
 
 (provide 'prelude-lang-lisp)
