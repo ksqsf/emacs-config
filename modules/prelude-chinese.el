dbb5f059a60fb00b2cd75ccb3e7a0628dc0e64d0
ksqsf <i@ksqsf.moe>
2019-10-14 11:45:28 +0800

    Fix Chinese fonts on Mac

--- modules/prelude-chinese.el
+++ modules/prelude-chinese.el
@@ -1,14 +1,14 @@
 ;;; -*- lexical-binding: t; -*-
 ;; Table font for perfect alignment.
 
-;; TODO: need work on mac
-
 ;; Warning: this may not work on different font settings.
-;; (eval-after-load 'org
-;;   '(set-face-font 'org-table "Iosevka Term-11"))
+(with-eval-after-load 'org
+  (set-face-font 'org-table "Iosevka Term-14")
+  (set-face-font 'org-todo "Iosevka Term-14")
+  (set-face-font 'org-done "Iosevka Term-14"))
 
-;; (eval-after-load 'markdown
-;;   '(set-face-font 'markdown-table-face "Iosevka Term-11"))
+(eval-after-load 'markdown
+  '(set-face-font 'markdown-table-face "Iosevka Term-14"))
 
 ;; Even though I speak Chinese, please use English dictionary for
 ;; spell checking.
