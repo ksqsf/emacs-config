e3bf176c562efb5d79a12ebd05eba4d9b8e86e5c
ksqsf <i@ksqsf.moe>
2019-10-26 22:47:57 +0800

    [pest-mode] baby step indent

--- lisp/pest-mode.el
+++ lisp/pest-mode.el
@@ -23,21 +23,58 @@
 ;;; Commentary:
 
 ;; This package provides GNU Emacs major modes for editing Pest grammar files.
-;; Currently, it supports syntax highlighting.
+;; Currently, it supports syntax highlighting and indentation.
 
 ;;; Code:
 
 (require 'rx)
 
-(setq pest--highlights
-      `((,(rx (or "SOI" "EOI" "@" "+" "*" "~")) . font-lock-keyword-face)
-        (,(rx "//" (* nonl) eol)            . font-lock-comment-face)
-        (,(rx "'" (char alpha) "'")         . font-lock-string-face)
-        (,(rx (+ (or alpha "_")) (* (or (char alnum) "_"))) . font-lock-variable-name-face)
-        ))
+(defvar pest--highlights
+  `((,(rx (or "SOI" "EOI" "@" "+" "*" "?" "~"))         . font-lock-keyword-face)
+    (,(rx "//" (* nonl) eol)                            . font-lock-comment-face)
+    (,(rx "'" (char alpha) "'")                         . font-lock-string-face)
+    (,(rx (+ (or alpha "_")) (* (or (char alnum) "_"))) . font-lock-variable-name-face)))
+
+(defun pest-indent-line (&optional indent)
+  "Indent the current line according to the Pest syntax."
+  (interactive "P")
+  (let ((pos (- (point-max) (point)))
+        (indent (or indent (calculate-pest-indentation)))
+        (shift-amt nil)
+        (beg (progn (beginning-of-line) (point))))
+    (skip-chars-forward " \t")
+    (if (null indent)
+        (goto-char (- (point-max) pos))
+      (setq shift-amt (- indent (current-column)))
+      (if (zerop shift-amt)
+          nil
+        (delete-region beg (point))
+        (indent-to indent))
+      (if (> (- (point-max) pos) (point))
+          (goto-char (- (point-max) pos))))))
+
+(defun calculate-pest-indentation ()
+  (let (indent)
+    (save-excursion
+      (back-to-indentation)
+      (let* ((ppss (syntax-ppss))
+             (depth (car ppss))
+             (paren-start-pos (cadr ppss))
+             (base (* 4 depth))
+             (rule-sep (save-excursion
+                         (or (looking-at "|")
+                             (re-search-backward "|" paren-start-pos t)))))
+        (unless (= depth 0)
+          (setq indent base)
+          (if (looking-at "}")
+              (setq indent (- base 4))
+            (if (null rule-sep)
+              (setq indent (+ 2 base)))))))
+    indent))
 
 (define-derived-mode pest-mode prog-mode "Pest"
   "Major mode for editing Pest files"
-  (setq font-lock-defaults '(pest--highlights)))
+  (setq-local font-lock-defaults '(pest--highlights))
+  (setq-local indent-line-function #'pest-indent-line))
 
 (provide 'pest-mode)
