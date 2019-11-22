8f7135f3651653e710ccacbda357ddb9f13f25e5
ksqsf <i@ksqsf.moe>
2019-11-22 11:26:51 +0800

    [volatile/scratch] make a command

--- volatile/00-scratch.el
+++ volatile/00-scratch.el
@@ -1,13 +1,18 @@
-(make-process :name "fortune"
-              :command '("fortune")
-              :filter
-              (lambda (process output)
-                (with-current-buffer "*scratch*"
-                  (fundamental-mode)
-                  (erase-buffer)
-                  (insert output)
-                  (insert "\nHappy Hacking!\nDon't forget to check your calendar (C-c a)\n\n")
-                  (lisp-interaction-mode)
-                  (comment-region (point-min) (point-max))
-                  (require 'ansi-color)
-                  (ansi-color-apply-on-region (point-min) (point-max)))))
+(defun refresh-scratch ()
+  "Refresh the *scratch* buffer with a fortune cookie."
+  (interactive)
+  (make-process :name "fortune"
+                :command '("fortune")
+                :filter
+                (lambda (process output)
+                  (with-current-buffer "*scratch*"
+                    (fundamental-mode)
+                    (erase-buffer)
+                    (insert output)
+                    (insert "\nHappy Hacking!\nDon't forget to check your calendar (C-c a)\n\n")
+                    (lisp-interaction-mode)
+                    (comment-region (point-min) (point-max))
+                    (require 'ansi-color)
+                    (ansi-color-apply-on-region (point-min) (point-max))))))
+
+(add-hook 'after-init-hook #'refresh-scratch)
