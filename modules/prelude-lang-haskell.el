bd3722c44a7c28a6c5bf5a9d3e18ee54a181c026
ksqsf <i@ksqsf.moe>
2019-12-15 16:03:08 +0800

    [prog] update rust and haskell to lsp-based solution

--- modules/prelude-lang-haskell.el
+++ modules/prelude-lang-haskell.el
@@ -5,4 +5,14 @@
   :mode ("\\.hs\\'" . haskell-mode)
   :hook (haskell-mode . intero-mode))
 
+;; (use-package haskell-mode
+;;   :mode ("\\.hs\\'" . haskell-mode))
+
+;; (use-package dante
+;;   :after haskell-mode
+;;   :commands 'dante-mode
+;;   :init
+;;   (add-hook 'haskell-mode-hook 'flycheck-mode)
+;;   (add-hook 'haskell-mode-hook 'dante-mode))
+
 (provide 'prelude-lang-haskell)
