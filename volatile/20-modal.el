de0d97e5c8934210e6e9f252f9cbb424da97e208
ksqsf <i@ksqsf.moe>
2020-01-06 03:50:38 +0800

    [volatile/modal] try xah-fly-keys and boon and disable them

---
+++ volatile/20-modal.el
@@ -0,0 +1,12 @@
+(use-package xah-fly-keys
+  :disabled
+  :config
+  (xah-fly-keys-set-layout "qwerty")
+  (xah-fly-keys 1)
+  (global-set-key (kbd "<escape>") 'xah-fly-command-mode-activate))
+
+(use-package boon
+  :disabled
+  :config
+  (require 'boon-qwerty)
+  (boon-mode))
