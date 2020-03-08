8879d374f921ae860235fd47cc58af14fb8fdf00
ksqsf <i@ksqsf.moe>
2020-03-09 03:27:16 +0800

    [lang/rust] revise

--- modules/prelude-lang-rust.el
+++ modules/prelude-lang-rust.el
@@ -4,33 +4,14 @@
 (use-package rustic
   :mode ("\\.rs\\'" . rustic-mode)
   :config
+  (setq rustic-analyzer-command "~/.cargo/bin/rust-analyzer")
   (define-key rustic-mode-map (kbd "[") (double-tap-to-insert ?\())
   (define-key rustic-mode-map (kbd "]") (double-tap-to-insert ?\)))
   (add-hook 'rustic-mode-hook #'yas-minor-mode)
   (add-hook 'rustic-mode-hook #'subword-mode)
   (add-hook 'rustic-mode-hook #'smartparens-mode)
-  (add-hook 'rustic-mode-hook #'(lambda () (setq company-idle-delay 0.1)))
+  (add-hook 'rustic-mode-hook #'(lambda () (setq company-idle-delay nil)))
   (add-hook 'rustic-mode-hook #'electric-pair-mode))
-
-;; (use-package rust-mode
-;;   :mode ("\\.rs\\'" . rust-mode)
-;;   :config
-;; (define-key rust-mode-map (kbd "[") (double-tap-to-insert ?\())
-;; (define-key rust-mode-map (kbd "]") (double-tap-to-insert ?\)))
-;; (add-hook 'rust-mode-hook #'yas-minor-mode)
-;; (add-hook 'rust-mode-hook #'subword-mode)
-;; (add-hook 'rust-mode-hook #'(lambda () (setq company-idle-delay nil))))
-
-;; (use-package cargo
-;;   :hook (rust-mode . cargo-minor-mode)
-;;   :config
-;;   (add-hook 'find-file-hook
-;;             (lambda ()
-;;               (when (string= (file-name-nondirectory buffer-file-name) "Cargo.toml")
-;;                 (cargo-minor-mode)))))
-
-;; (use-package racer
-;;   :hook (rust-mode . racer-mode))
 
 (use-package ob-rust
   :after org)
