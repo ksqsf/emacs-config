dc2c3c396f7d186ef2a2f1a75cdb17cae1e84d7c
ksqsf <i@ksqsf.moe>
2020-03-10 20:25:51 +0800

    [lisp/treefold] initial

---
+++ lisp/treefold.el
@@ -0,0 +1,134 @@
+;;; treefold.el --- Fold tree-format texts as if it is a tree -*- lexical-binding: t; -*-
+
+;; Author: ksqsf <i@ksqsf.moe>
+;; URL: https://github.com/ksqsf/emacs-config
+;; Version: 0.1
+;; Package-Requires: ((emacs "26.3"))
+
+;; Copyright (C) 2020 ksqsf
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; treefold.el provides a minor mode, which contains commands to help
+;; you fold any text that looks like a tree, e.g. the output of
+;; tree(1) or that of cargo-tree.
+
+;; For instance, the output of tree(1):
+
+;; .
+;; ├── Cargo.lock
+;; ├── Cargo.toml
+;; ├── deps
+;; └── src
+;;     ├── errors.rs
+;;     └── main.rs
+
+;; If you press Return on the line "src", you will fold the
+;; contents of "src", and get the result:
+
+;; .
+;; ├── Cargo.lock
+;; ├── Cargo.toml
+;; ├── deps
+;; └── src [...]
+
+;;; Code:
+
+(defun treefold--subtree-end ()
+  (save-excursion
+    (let (end (col (current-column)))
+      (while (and (= (forward-line) 0)
+                  (= (move-to-column col) col)
+                  (looking-at "[ |]"))
+        nil)
+      (1+ (point-at-eol 0)))))
+
+(defun treefold--folded-line (start end)
+  (save-excursion
+    (goto-char start)
+    (format "%s [...]\n" (buffer-substring start (point-at-eol)))))
+
+(defun treefold--fold-subtree (start end)
+  "Fold a subtree, which spans region from START to END."
+  (save-excursion
+    (goto-char start)
+    (let* ((eol (point-at-eol))
+           (line-overlay (make-overlay start eol))
+           (hide-overlay (make-overlay (1+ eol) end)))
+      (if (>= (1+ eol) end)
+          ;; There's nothing to hide.
+          (progn
+            (delete-overlay line-overlay)
+            (delete-overlay hide-overlay))
+        (overlay-put line-overlay 'after-string " [...]")
+        (overlay-put line-overlay 'treefold 'line)
+        (overlay-put hide-overlay 'invisible t)
+        (overlay-put hide-overlay 'treefold 'hide)))))
+
+(defun treefold--unfold-in-region (start end)
+  "Unfold all subtrees in region (START, END)."
+  (remove-overlays start end))
+
+(defun treefold--fold ()
+  "Fold the subtree found on this line."
+  (beginning-of-line)
+  (unless (re-search-forward "\\+" (point-at-eol) t 1)
+    (error "This line doesn't have a subtree."))
+  (let* ((start (point-at-bol))
+         (end (treefold--subtree-end)))
+    (treefold--unfold-in-region start end)
+    (treefold--fold-subtree start end)))
+
+(defun treefold--line-overlay (pos)
+  "Check if the current line is part of a folded subtree."
+  (catch 'return
+    (dolist (overlay (overlays-at (point)))
+      (when (eq 'line (overlay-get overlay 'treefold))
+        (throw 'return overlay)))
+    (throw 'return nil)))
+
+(defun treefold-toggle ()
+  "Toggle whether the current subtree is folded."
+  (interactive)
+  (save-excursion
+    (let ((line-overlay (treefold--line-overlay (point))))
+      (if line-overlay
+          (treefold--unfold line-overlay)
+        (treefold--fold)))))
+
+(defun treefold--unfold (line-overlay)
+  "Unfold the subtree found on this line."
+  ;; Delete line-overlay.
+  (delete-overlay line-overlay)
+  ;; Delete hide-overlay.
+  (let ((beg (1+ (point-at-eol))))
+    (dolist (overlay (overlays-at beg))
+      (when (eq 'hide (overlay-get overlay 'treefold))
+        (delete-overlay overlay)))))
+
+(defun treefold-unfold-all ()
+  (interactive)
+  (treefold--unfold-in-region (point-min) (point-max)))
+
+(define-minor-mode treefold-mode
+  "Enable treefold functions in this buffer."
+  :keymap (let ((keymap (make-sparse-keymap)))
+            (define-key keymap (kbd "<RET>") #'treefold-toggle)
+            keymap))
+
+(provide 'treefold)
+
+;;; treefold.el ends here.
