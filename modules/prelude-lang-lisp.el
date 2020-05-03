ebb3e0f618a2698c56aba0979eff21ad69988a46
ksqsf <i@ksqsf.moe>
2020-05-03 21:41:14 +0800

    Add SLY

--- modules/prelude-lang-lisp.el
+++ modules/prelude-lang-lisp.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: t; -*-
 
+(setq inferior-lisp-program "ccl64")
+
 (use-package paredit
   :hook ((lisp-mode emacs-lisp-mode lisp-interaction-mode) . paredit-mode)
   :config
@@ -8,7 +10,7 @@
 (eval-after-load 'scheme
   (setq scheme-program-name "mechanics"))
 
-(use-package geiser
+(use-package geiserb
   :mode (("\\.ss\\'" . geiser-mode))
   :config
   (setq geiser-default-implementation 'chez))
@@ -16,5 +18,9 @@
 (defun mechanics ()
   (interactive)
   (run-scheme "/usr/local/scmutils/mit-scheme/bin/scheme --library /usr/local/scmutils/mit-scheme/lib"))
+
+(use-package sly
+  :mode (("\\.lisp\\'" . sly-mode))
+  :commands (sly-mode))
 
 (provide 'prelude-lang-lisp)
