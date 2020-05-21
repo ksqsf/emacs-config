7310bcdd67efa409d9df75b4da4347b1a277c4c6
ksqsf <i@ksqsf.moe>
2020-05-21 20:32:11 +0800

    [tex] Enable reftex.

--- modules/prelude-tex.el
+++ modules/prelude-tex.el
@@ -1,17 +1,21 @@
 ;;; -*- lexical-binding: t; -*-
 
 (use-package tex
-  :defer t
   :ensure auctex
   :mode (("\\.tex\\'" . LaTeX-mode))
   :config
   (setq-default TeX-engine 'xetex)
   (add-hook 'LaTeX-mode-hook
-            (lambda ()
-              (setq fill-column 80)
-              (setq TeX-master (expand-file-name
-                                "main.tex"
-                                (projectile-project-root))))))
+            #'(lambda ()
+                (setq fill-column 80)
+                (setq TeX-master (expand-file-name
+                                  "main.tex"
+                                  (projectile-project-root))))))
+
+(use-package reftex
+  :hook (LaTeX-mode . reftex-mode)
+  :config
+  (setq reftex-plug-into-AUCTeX t))
 
 (use-package cdlatex
   :hook ((latex-mode LaTeX-mode) . turn-on-cdlatex)
