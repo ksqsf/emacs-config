7dc8f890ad6cddd8893ed28f60343897a27d7d56
ksqsf <i@ksqsf.moe>
2020-08-25 09:47:50 +0800

    [package] prefer HTTP to HTTPS

--- modules/prelude-package.el
+++ modules/prelude-package.el
@@ -5,10 +5,10 @@
 ;;; * No version pinning
 
 ;; Package Archive URL
-(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("melpa-stable" . "https://mirrors.ustc.edu.cn/elpa/melpa-stable/")
-                         ("org" . "https://mirrors.ustc.edu.cn/elpa/org/")))
+(setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
+                         ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
 (package-initialize)
 
 ;; use-package
