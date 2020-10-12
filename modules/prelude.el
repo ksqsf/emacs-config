36d0189f11a985086b3478aee06cfeea5dd17490
ksqsf <i@ksqsf.moe>
2020-10-12 11:31:41 +0800

    trivial

--- modules/prelude.el
+++ modules/prelude.el
@@ -8,7 +8,7 @@
 
 (defmacro with-suppressed-message (&rest body)
   "Suppress new messages temporarily in the echo area and the
-  `*Messages*` buffer while BODY is evaluated."
+*Messages* buffer while BODY is evaluated."
   (declare (indent 0))
   (let ((message-log-max nil))
     `(with-temp-message (or (current-message) "") ,@body)))
