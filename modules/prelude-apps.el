f659b08f2dce5c11fefa0699a202f1ffff16b790
ksqsf <i@ksqsf.moe>
2020-11-09 23:46:56 +0800

    treemacs fancy

--- modules/prelude-apps.el
+++ modules/prelude-apps.el
@@ -109,8 +109,81 @@
   (require 'org-roam-protocol))
 
 (use-package treemacs
+  :bind ("C-c t" . treemacs)
   :commands (treemacs)
   :config
-  (add-hook 'treemacs-mode-hook #'variable-pitch-mode))
+  ;; Add the name of the current workspace to the mode line
+  (defun prelude-treemacs-mode-line ()
+    '(" Treemacs " (:eval)))
+  (setq treemacs-user-mode-line-format '(" Treemacs [" (:eval (treemacs-workspace->name (treemacs-current-workspace))) "] "))
+
+  ;;
+  (setq prelude-treemacs-default-workspace "Default")
+
+  (defun -treemacs-get-workspace (name)
+    (let ((workspaces (->> treemacs--workspaces
+                           (--reject (eq it (treemacs-current-workspace)))
+                           (--map (cons (treemacs-workspace->name it) it)))))
+      (cdr (--first (string= (car it) name) workspaces))))
+
+  (defun -treemacs-create-workspace (name)
+    (treemacs-block
+     (treemacs-return-if (treemacs--is-name-invalid? name)
+       `(invalid-name ,name))
+     (-when-let (ws (--first (string= name (treemacs-workspace->name it))
+                             treemacs--workspaces))
+       (treemacs-return `(duplicate-name ,ws)))
+     (-let [workspace (treemacs-workspace->create! :name name)]
+       (add-to-list 'treemacs--workspaces workspace :append)
+       (treemacs--persist)
+       (run-hook-with-args 'treemacs-create-workspace-functions workspace)
+       `(success ,workspace))))
+
+  (defun -treemacs-get-or-create-workspace (name)
+    (or (--first (string= name (treemacs-workspace->name it))
+                 treemacs--workspaces)
+        (let (res (-treemacs-create-workspace name))
+          (if (equal (car res) 'success)
+              (cdr res)
+            (error "Couldn't create workspace")))))
+
+  (defun -treemacs-switch-to-workspace (ws)
+    (setf (treemacs-current-workspace) ws)
+    (treemacs--invalidate-buffer-project-cache)
+    (treemacs--rerender-after-workspace-change)
+    (run-hooks 'treemacs-switch-workspace-hook))
+
+  (defun -treemacs-which-workspace ()
+    "Which workspace does the current file belong to?"
+    (--first (treemacs-is-path (buffer-file-name) :in-workspace it) (treemacs-workspaces)))
+
+  (setq current-file-ws (-treemacs-which-workspace))
+  (setq default-ws (-treemacs-get-or-create-workspace prelude-treemacs-default-workspace))
+
+  (defun -treemacs-add-project-to-workspace-or-switch ()
+    "Add the current project to the default workspace, or locate it if it's already known in a workspace.
+
+The default workspace is specificied by `prelude-treemacs-default-workspace'"
+    (let (current-file-ws (-treemacs-which-workspace))
+      (if current-file-ws
+          (progn
+            (-treemacs-switch-to-workspace current-file-ws)
+            (treemacs--follow))
+        (let ((default-ws (-treemacs-get-or-create-workspace prelude-treemacs-default-workspace)))
+          (message "I'm here!")
+          (-treemacs-switch-to-workspace default-ws)
+          (treemacs-display-current-project-exclusively)))))
+
+  ;; (defun -treemacs-locate-project-if-in-workspace ()
+  ;;   (let ((ws (-treemacs-which-workspace)))
+  ;;     (when (and (not (null ws))
+  ;;                (not (eq ws (treemacs-current-workspace))))
+  ;;       (-treemacs-switch-to-workspace ws))))
+
+  (add-hook 'treemacs-mode-hook #'variable-pitch-mode)
+  (add-hook 'projectile-after-switch-project-hook #'-treemacs-add-project-to-workspace-or-switch)
+  ;; (add-hook 'buffer-list-update-hook #'-treemacs-locate-project-if-in-workspace)
+
+  (use-package treemacs-projectile))
 
 (provide 'prelude-apps)
