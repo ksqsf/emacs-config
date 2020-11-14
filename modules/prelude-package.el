88f7251f68739c829b8e8c86f644fe4041e9e5ad
ksqsf <i@ksqsf.moe>
2020-11-15 00:44:46 +0800

    [package] remove melpa-stable

--- modules/prelude-package.el
+++ modules/prelude-package.el
@@ -5,9 +5,9 @@
 ;;; * No version pinning
 
 ;; Package Archive URL
+
 (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
                          ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
 (package-initialize)
 
