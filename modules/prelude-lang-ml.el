caced9df39083427a9fe8841a1fef40fff0634c8
ksqsf <i@ksqsf.moe>
2020-12-23 20:50:41 +0800

    [lang/ml] Add support for OCaml

--- modules/prelude-lang-ml.el
+++ modules/prelude-lang-ml.el
@@ -1,7 +1,17 @@
 ;;; -*- lexical-binding: t; -*-
 ;; Tuareg for OCaml
+
 (use-package tuareg
-  :commands (ocaml-mode)
-  :mode (("\\.ml\\'" . ocaml-mode)))
+  :mode ("\\.ml[4ilpy]?$" . tuareg-mode))
+
+(use-package dune :after tuareg)
+
+(use-package utop
+  :after tuareg
+  :bind (:map tuareg-mode-map)
+  :when (executable-find "utop"))
+
+(use-package merlin
+  :hook (tuareg-mode . merlin-mode))
 
 (provide 'prelude-lang-ml)
