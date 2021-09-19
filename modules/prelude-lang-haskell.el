b45c30424a80cd574476c7a1fa401d9f5ac9211f
ksqsf <i@ksqsf.moe>
2021-09-19 22:58:14 +0800

    [lang/haskell] various updates

--- modules/prelude-lang-haskell.el
+++ modules/prelude-lang-haskell.el
@@ -1,8 +1,71 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; Currently, only haskell-mode is supported.
+
 (use-package haskell-mode
   :mode ("\\.hs\\'" . haskell-mode)
-  :hook (haskell-mode-hook . yas-minor-mode))
+  :bind (("C-c C-l" . haskell-process-load-or-reload)
+         ("C-c C-f" . haskell-mode-format-imports)
+         ("<f8>" . haskell-navigate-imports))
+  :hook (haskell-mode . yas-minor-mode)
+  :hook (haskell-mode . interactive-haskell-mode)
+  ;; :hook (haskell-mode . flyspell-prog-mode)
+  :config
+  (defun hpack-after-save-hook ()
+    (when (string= (buffer-name) "package.yaml")
+      (start-process "hpack" " *hpack*" "hpack")))
+  (add-hook 'after-save-hook #'hpack-after-save-hook)
+
+  (with-eval-after-load 'align
+    (add-to-list 'align-rules-list
+                 '(haskell-types
+                   (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+                   (modes quote (haskell-mode literate-haskell-mode))))
+    (add-to-list 'align-rules-list
+                 '(haskell-assignment
+                   (regexp . "\\(\\s-+\\)=\\s-+")
+                   (modes quote (haskell-mode literate-haskell-mode))))
+    (add-to-list 'align-rules-list
+                 '(haskell-arrows
+                   (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+                   (modes quote (haskell-mode literate-haskell-mode))))
+    (add-to-list 'align-rules-list
+                 '(haskell-left-arrows
+                   (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+                   (modes quote (haskell-mode literate-haskell-mode)))))
+
+  (setq haskell-process-wrapper-function
+        (lambda (args) (apply 'nix-shell-command (nix-current-sandbox) args)))
+
+  (with-eval-after-load 'speedbar
+      (speedbar-add-supported-extension ".hs")))
+
+(use-package w3m-haddock
+  :ensure w3m
+  :after haskell-mode
+  :config
+  (setq w3m-mode-map (make-sparse-keymap))
+
+  (define-key w3m-mode-map (kbd "RET") 'w3m-view-this-url)
+  (define-key w3m-mode-map (kbd "q") 'bury-buffer)
+  (define-key w3m-mode-map (kbd "<mouse-1>") 'w3m-maybe-url)
+  (define-key w3m-mode-map [f5] 'w3m-reload-this-page)
+  (define-key w3m-mode-map (kbd "C-c C-d") 'haskell-w3m-open-haddock)
+  (define-key w3m-mode-map (kbd "M-<left>") 'w3m-view-previous-page)
+  (define-key w3m-mode-map (kbd "M-<right>") 'w3m-view-next-page)
+  (define-key w3m-mode-map (kbd "M-.") 'w3m-haddock-find-tag)
+
+  (defun w3m-maybe-url ()
+    (interactive)
+    (if (or (equal '(w3m-anchor) (get-text-property (point) 'face))
+            (equal '(w3m-arrived-anchor) (get-text-property (point) 'face)))
+        (w3m-view-this-url)))
+  
+  (add-hook 'w3m-display-hook 'w3m-haddock-display))
+
+(defun pointfree (expr)
+  "Get the point-free version of EXPR from http://pointfree.io/."
+  (interactive))
 
 ;; https://github.com/digital-asset/ghcide
 ;; https://github.com/haskell/haskell-ide-engine#using-hie-with-emacs
@@ -12,124 +75,14 @@
   :config
   (setq lsp-haskell-process-path-hie "hie-wrapper"))
 
-;; Intero is discontinued (Nov 2019)
-(use-package intero
-  :disabled
-  :hook (haskell-mode . intero-mode)
-  :config
-  ;; See https://github.com/chrisdone/intero/pull/643
-  (defun intero-get-info-of (thing)
-    "Get info for THING."
-    (let ((optimistic-result
-           (replace-regexp-in-string
-            "\n$" ""
-            (intero-blocking-call
-             'backend
-             (format ":info %s" thing)))))
-      (if (string-match-p "^<interactive>" optimistic-result)
-          ;; Load the module Interpreted so that we get information,
-          ;; then restore bytecode.
-          (progn (intero-async-call
-                  'backend
-                  ":set -fbyte-code")
-                 (set-buffer-modified-p t)
-                 (save-buffer)
-                 (unless (member 'save flycheck-check-syntax-automatically)
-                   (intero-async-call
-                    'backend
-                    (concat ":load " (intero-path-for-ghci (intero-temp-file-name)))))
-                 (mapc
-                  (lambda (flag)
-                    (intero-async-call
-                     'backend
-                     (concat ":set " flag)))
-                  (intero-ghci-output-flags))
-                 (replace-regexp-in-string
-                  "\n$" ""
-                  (intero-blocking-call
-                   'backend
-                   (format ":info %s" thing))))
-        optimistic-result)))
-
-  (defun intero-start-process-in-buffer (buffer &optional targets source-buffer stack-yaml)
-    "Start an Intero worker in BUFFER.
-Uses the specified TARGETS if supplied.
-Automatically performs initial actions in SOURCE-BUFFER, if specified.
-Uses the default stack config file, or STACK-YAML file if given."
-    (if (buffer-local-value 'intero-give-up buffer)
-        buffer
-      (let* ((process-info (intero-start-piped-process buffer targets stack-yaml))
-             (arguments (plist-get process-info :arguments))
-             (options (plist-get process-info :options))
-             (process (plist-get process-info :process)))
-        (set-process-query-on-exit-flag process nil)
-        (mapc
-         (lambda (flag)
-           (process-send-string process (concat ":set " flag "\n")))
-         (intero-ghci-output-flags))
-        (process-send-string process ":set -fdefer-type-errors\n")
-        (process-send-string process ":set -fdiagnostics-color=never\n")
-        (process-send-string process ":set prompt \"\\4\"\n")
-        (with-current-buffer buffer
-          (erase-buffer)
-          (when stack-yaml
-            (setq intero-stack-yaml stack-yaml))
-          (setq intero-targets targets)
-          (setq intero-start-time (current-time))
-          (setq intero-source-buffer source-buffer)
-          (setq intero-arguments arguments)
-          (setq intero-starting t)
-          (setq intero-callbacks
-                (list (list (cons source-buffer
-                                  buffer)
-                            (lambda (buffers msg)
-                              (let ((source-buffer (car buffers))
-                                    (process-buffer (cdr buffers)))
-                                (with-current-buffer process-buffer
-                                  (when (string-match "^Intero-Service-Port: \\([0-9]+\\)\n" msg)
-                                    (setq intero-service-port (string-to-number (match-string 1 msg))))
-                                  (setq-local intero-starting nil))
-                                (when source-buffer
-                                  (with-current-buffer source-buffer
-                                    (when flycheck-mode
-                                      (run-with-timer 0 nil
-                                                      'intero-call-in-buffer
-                                                      (current-buffer)
-                                                      'intero-flycheck-buffer)))))
-                              (message "Booted up intero!"))))))
-        (set-process-filter
-         process
-         (lambda (process string)
-           (when intero-debug
-             (message "[Intero] <- %s" string))
-           (when (buffer-live-p (process-buffer process))
-             (with-current-buffer (process-buffer process)
-               (goto-char (point-max))
-               (insert string)
-               (when (and intero-try-with-build
-                          intero-starting)
-                 (let ((last-line (buffer-substring-no-properties
-                                   (line-beginning-position)
-                                   (line-end-position))))
-                   (if (string-match-p "^Progress" last-line)
-                       (message "Booting up intero (building dependencies: %s)"
-                                (downcase
-                                 (or (car (split-string (replace-regexp-in-string
-                                                         "\u0008+" "\n"
-                                                         last-line)
-                                                        "\n" t))
-                                     "...")))
-                     (message "Booting up intero ..."))))
-               (intero-read-buffer)))))
-        (set-process-sentinel process 'intero-sentinel)
-        buffer)))
-
-  (define-advice intero-ghci-output-flags (:override ())
-    (with-current-buffer (intero-buffer 'backend)
-      (let ((current-version (mapcar #'string-to-number (split-string (intero-ghc-version) "\\."))))
-        (if (intero-version>= current-version '(8 4 1))
-            '("-fno-code" "-fwrite-interface")
-          '("-fobject-code"))))))
+(use-package dante
+  :disabled ;; not very useful. haskell-interactive-mode is good enough.
+  :after haskell-mode
+  :commands 'dante-mode
+  :init
+  (add-hook 'haskell-mode-hook 'flymake-mode)
+  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+  (add-hook 'haskell-mode-hook 'dante-mode))
 
 (defvar *prelude--latest-stackage-lts-version* nil
   "Cached value for stackage lts version.")
