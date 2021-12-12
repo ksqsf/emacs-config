9d1ad081a547a50811c8ef6766a669baa80f38c8
ksqsf <i@ksqsf.moe>
2021-12-12 14:23:09 +0800

    [project] `project-shell' should run vterm

--- modules/prelude-project.el
+++ modules/prelude-project.el
@@ -26,4 +26,22 @@
   :after (projectile)
   :commands (counsel-projectile-mode))
 
+(use-package project
+  :config
+  (defun project-shell ()
+  "Start an inferior shell in the current project's root directory.
+If a buffer already exists for running a shell in the project's root,
+switch to it.  Otherwise, create a new shell buffer.
+With \\[universal-argument] prefix arg, create a new inferior shell buffer even
+if one already exists.
+
+Modified to run vterm instead of shell."
+  (interactive)
+  (let* ((default-directory (project-root (project-current t)))
+         (default-project-shell-name (project-prefixed-buffer-name "vterm"))
+         (shell-buffer (get-buffer default-project-shell-name)))
+    (if (and shell-buffer (not current-prefix-arg))
+        (pop-to-buffer-same-window shell-buffer)
+      (vterm-other-window (generate-new-buffer-name default-project-shell-name))))))
+
 (provide 'prelude-project)
