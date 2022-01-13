63474975f42d3aacf18800472b78663a882ac42c
ksqsf <i@ksqsf.moe>
2022-01-13 21:22:57 +0800

    [volatile] lean

---
+++ volatile/50-lean.el
@@ -0,0 +1,3 @@
+(setq load-path (cons (expand-file-name "~/src/Clone/lean4/lean4-mode") load-path))
+;; (setq lean4-mode-required-packages '(dash f flycheck lsp-mode magit-section s))
+(require 'lean4-mode)
