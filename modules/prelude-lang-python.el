78229935591062ef971b3bf2dfd9bbf2289e2d98
ksqsf <i@ksqsf.moe>
2022-03-04 23:14:37 +0800

    [lang/python] override C-c C-c to send buffer; C-c C-p to run python

--- modules/prelude-lang-python.el
+++ modules/prelude-lang-python.el
@@ -11,6 +11,8 @@
   :hook
   (python-mode . k|lsp-ensure)
   :bind (:map python-mode-map
+              ("C-c C-p" . run-python)
+              ("C-c C-c" . python-shell-send-buffer)
               ("C-x C-e" . python-shell-send-statement))
   :config
   ;; (add-hook 'inferior-python-mode-hook
