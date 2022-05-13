bf532482c93950d0e4e3463d8d0bf0a7a3a01900
ksqsf <i@ksqsf.moe>
2022-05-13 17:37:33 +0800

    [volatile] delete lean-mode.

--- volatile/50-lean.el
+++
@@ -1,3 +0,0 @@
-(setq load-path (cons (expand-file-name "~/src/Clone/lean4/lean4-mode") load-path))
-;; (setq lean4-mode-required-packages '(dash f flycheck lsp-mode magit-section s))
-(require 'lean4-mode)
