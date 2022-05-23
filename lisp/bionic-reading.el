caf8adbc584d496c1cc147e90e91fa207f1e9021
ksqsf <i@ksqsf.moe>
2022-05-23 23:33:06 +0800

    [lisp/bionic-reading] add

---
+++ lisp/bionic-reading.el
@@ -0,0 +1,72 @@
+;;; bionic-reading.el --- Bionic reading             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  ksqsf
+
+;; Author: ksqsf <i@ksqsf.moe>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package implements something similar to bionic reading in Emacs.
+;;
+;; Note that bionic reading is patented.
+
+;;; Code:
+
+(defvar-local bionic-overlays nil)
+
+(defun bionic-word ()
+  "Bionicify the word at point"
+  (interactive)
+  (let* ((bounds (bounds-of-thing-at-point 'word))
+         (beg (car bounds))
+         (end (cdr bounds))
+         (whole-len (- end beg)))
+    (cond
+     ((>= whole-len 2)
+      (let* ((half-len (/ whole-len 2))
+             (real-len (if (or (> whole-len 6) (= whole-len 3))
+                           (+ half-len 1)
+                         half-len))
+             (ov (make-overlay beg (+ beg real-len))))
+        (overlay-put ov 'face 'bold)
+        (push ov bionic-overlays)))
+     ((> (- end beg) 1)
+      (let ((ov (make-overlay beg (+ beg 1))))
+        (overlay-put ov 'face 'bold)
+        (push ov bionic-overlays)))
+     (t nil))))
+
+(defun bionic-buffer ()
+  "Bionicify all the visible parts of the current buffer."
+  (interactive)
+  (if (not (null bionic-overlays))
+      (bionic-debuffer))
+  (save-excursion
+    (goto-char (point-min))
+    (while (not (= (point) (point-max)))
+      (if (looking-at "\\w")
+          (bionic-word))
+      (forward-to-word 1))))
+
+(defun bionic-debuffer ()
+  "Undo the bionicification."
+  (interactive)
+  (dolist (ov bionic-overlays)
+    (delete-overlay ov)))
+
+(provide 'bionic-reading)
+;;; bionic-reading.el ends here
