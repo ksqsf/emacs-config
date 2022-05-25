55608c637093ed13446ed21917a365af272a81a2
ksqsf <i@ksqsf.moe>
2022-05-25 15:24:08 +0800

    [early-init] remove `nongnu' package archive

--- early-init.el
+++ early-init.el
@@ -3,7 +3,6 @@
 (setq gc-cons-threshold most-positive-fixnum)
 
 (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("nongnu" . "http://mirrors.ustc.edu.cn/elpa/nongnu")))
+                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")))
 
 (setq buffer-file-coding-system 'utf-8-unix)
