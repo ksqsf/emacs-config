8e227cb88666c13eafb373c3e2c83eb477de306f
ksqsf <i@ksqsf.moe>
2022-11-18 18:16:01 +0800

    Enable NonGNU ELPA.

--- early-init.el
+++ early-init.el
@@ -3,6 +3,7 @@
 (setq gc-cons-threshold most-positive-fixnum)
 
 (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.ustc.edu.cn/elpa/nongnu/")
                          ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")))
 
 (setq buffer-file-coding-system 'utf-8-unix)
