f5cd00caa5c2f8668d3fdc77c5d9a5a01f808fbf
ksqsf <i@ksqsf.moe>
2022-12-07 00:09:27 +0800

    [git] Add a primitive org-protocol integration. (WIP)

--- modules/prelude-git.el
+++ modules/prelude-git.el
@@ -8,4 +8,18 @@
   :after magit
   :defer t)
 
+;; Streamline the process of contributing to GitHub repositories.
+;; Doesn't seem to work.
+;; Will write one using the Native Messaging API soon(TM).
+;; https://developer.chrome.com/docs/apps/nativeMessaging/
+;;
+;; (require 'org-protocol)
+;; (add-to-list 'org-protocol-protocol-alist
+;;              '(("git-clone"
+;;                 :protocol "git-clone"
+;;                 :function git-clone-protocol-handler)))
+;; (defun git-clone-protocol-handler (url)
+;;   (message "OK!!")
+;;   (magit-clone-regular url "~/src/Clone/" nil))
+
 (provide 'prelude-git)
