420b053111cb8bb3ccc7c9f5a0b198f8ec38b4a3
ksqsf <i@ksqsf.moe>
2022-12-07 00:11:43 +0800

    [volatile/telega] Enable the ad blocker.

--- volatile/99-telega.el
+++ volatile/99-telega.el
@@ -1,2 +1,4 @@
 (with-eval-after-load 'telega
-  (add-to-list 'telega-proxies '(:server "127.0.0.1" :port 7890 :enable t :type (:@type "proxyTypeHttp"))))
+  (add-to-list 'telega-proxies '(:server "127.0.0.1" :port 7890 :enable t :type (:@type "proxyTypeHttp")))
+  (require 'telega-adblock)
+  (telega-adblock-mode 1))
