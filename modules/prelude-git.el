3de8a19b6f0332e4200c2c7c59714be827e609ca
ksqsf <i@ksqsf.moe>
2022-12-12 09:18:00 +0800

    [git] Add a command to quickly copy a reference URL (github only)

--- modules/prelude-git.el
+++ modules/prelude-git.el
@@ -30,4 +30,32 @@
 ;;     (vc-git-clone url org-protocol-git-clone-directory nil))
 ;;   nil)
 
+(defun github-parse-remote-url (remote)
+  "Parse a git remote hosted on github to a (user,repo) pair.
+
+This function returns nil if it cannot parse REMOTE."
+  (let ((ssh-regexp "git@github\\.com:\\(.*\\)/\\(.*\\)\\.git")
+        (https-regexp "https://github.com/\\(.*\\)/\\(.*\\)\\.git"))
+    (let ((maybe-ssh (string-match ssh-regexp remote)))
+      (if maybe-ssh
+          (cons (match-string-no-properties 1 remote) (match-string-no-properties 2 remote))
+        (let ((maybe-https (string-match https-regexp remote)))
+          (if maybe-https
+              (cons (match-string-no-properties 1 remote) (match-string-no-properties 2 remote))
+            nil))))))
+
+(defun github-copy-reference-url-at-point ()
+  "Copy a link to the current line on the GitHub Web interface."
+  (interactive)
+  (require 'magit)
+  (save-buffer)
+  (let* ((remote (magit-get-remote))
+         (remote-url (magit-git-str "remote" "get-url" remote))
+         (commit (magit-rev-parse "--short" "HEAD"))
+         (user/repo (github-parse-remote-url remote-url))
+         (relative-path (file-relative-name buffer-file-name (vc-git-root buffer-file-name)))
+         (url (format "https://github.com/%s/%s/blob/%s/%s#L%d" (car user/repo) (cdr user/repo) commit relative-path (line-number-at-pos))))
+    (kill-new url)
+    (message "%s" url)))
+
 (provide 'prelude-git)
