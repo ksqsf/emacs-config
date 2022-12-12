b86f2b067edca9fe3d876841c6270b888574ded6
ksqsf <i@ksqsf.moe>
2022-12-13 07:24:58 +0800

    ; Minor.

--- volatile/fish-protector.el
+++ volatile/fish-protector.el
@@ -2,19 +2,17 @@
 
 (setq fish-protector-alist
       '((:telega . (or (mode telega-root-mode)
-                       (mode telega-chat-mode)))
-        (:emacs . (project "\\.emacs\\.d"))))
+                       (mode telega-chat-mode)))))
 
-(fish-protector--group-rule-match-p '(:telega . (or (mode telega-root-mode)
-                                        (mode telega-chat-mode))))
-
-(fish-protector-add-reach-limit :telega
-                    (* 30 60)  ;; 30 minutes
-                    (lambda (cur-sec)
-                      (if (> cur-sec (* 60 60))  ;; hard limit: 60 minutes
-                          (progn
-                            (kill-buffer "*Telega Root*")
-                            (fish-protector-alert "Hard limit of Telega reached!"))
-                        (fish-protector-alert "Soft limit of Telega reached! Please go away from Telega!!"))))
+(fish-protector-add-reach-limit
+ :telega
+ (* 30 60)  ;; 30 minutes
+ (lambda (cur-sec)
+   (if (> cur-sec (* 60 60))  ;; hard limit: 60 minutes
+       (progn
+         (kill-buffer "*Telega Root*")
+         (fish-protector-alert "Hard limit of Telega reached!"))
+     (when (= 0 (% cur-sec (* 5 60)))
+       (fish-protector-alert "Soft limit of Telega reached! Please go away from Telega!!")))))
 
 (fish-protector-start)
