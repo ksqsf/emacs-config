777d766185f17ec5e9ee70155a8e6ac95256e9a6
ksqsf <i@ksqsf.moe>
2022-12-19 15:24:00 +0800

    ; Minor

--- modules/prelude-git.el
+++ modules/prelude-git.el
@@ -13,23 +13,6 @@
   :config
   (setq forge-owned-accounts '(("ksqsf" . (remote-name "personal")))))
 
-;; (require 'org-protocol)
-
-;; (setq org-protocol-protocol-alist
-;;       '(("git-clone"
-;;          :protocol "git-clone"
-;;          :function org-protocol-git-clone)))
-
-;; (defvar org-protocol-git-clone-directory 
-;;   "Default directory for `org-protocol-git-clone`.")
-
-;; (defun org-protocol-git-clone (info)
-;;   "Process an org-protocol://git-clone style url with INFO."
-;;   (let ((url (plist-get info :url)))
-;;     (message "%s" url)
-;;     (vc-git-clone url org-protocol-git-clone-directory nil))
-;;   nil)
-
 (defun github-parse-remote-url (remote)
   "Parse a git remote hosted on github to a (user,repo) pair.
 
