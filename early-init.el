4644c46c66e06257d86a0325e760290a04ce633d
ksqsf <i@ksqsf.moe>
2023-01-21 15:28:21 +0800

    [early-init] Change package archive URLs to HTTPS.
    To test if everything works.

--- early-init.el
+++ early-init.el
@@ -2,8 +2,8 @@
 
 (setq gc-cons-threshold most-positive-fixnum)
 
-(setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("nongnu" . "http://mirrors.ustc.edu.cn/elpa/nongnu/")
-                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")))
+(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")))
 
 (setq buffer-file-coding-system 'utf-8-unix)
