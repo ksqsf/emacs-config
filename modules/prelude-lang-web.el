60b8a86ec202eb445651d92edeb2f9d7047e86e5
ksqsf <i@ksqsf.moe>
2023-03-17 16:49:48 +0800

    [lang/web] Use the recommended config

--- modules/prelude-lang-web.el
+++ modules/prelude-lang-web.el
@@ -1,5 +1,12 @@
 (use-package web-mode
-  :mode ("\\.html?\\'" . web-mode)
+  :mode (("\\.html?\\'" . web-mode)
+         ("\\.phtml\\'" . web-mode)
+         ("\\.tpl\\.php\\'" . web-mode)
+         ("\\.[agj]sp\\'" . web-mode)
+         ("\\.as[cp]x\\'" . web-mode)
+         ("\\.erb\\'" . web-mode)
+         ("\\.mustache\\'" . web-mode)
+         ("\\.djhtml\\'" . web-mode))
   :config
   (setq web-mode-engines-alist
         '(("jinja" . "\\.html\\'"))))
