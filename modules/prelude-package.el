6b6be013381d76264ce9b4d9221162158af1458f
ksqsf <i@ksqsf.moe>
2023-03-20 23:02:02 +0800

    ; fix commit

--- modules/prelude-package.el
+++ modules/prelude-package.el
@@ -9,27 +9,12 @@
 ;; use-package
 (require 'use-package)
 
-;; NOTE: quelpa requires package, and hence package-quickstart will be
-;; useless.  (According to someone familiar with this matter.)
-(use-package quelpa
-  :ensure t
-  :demand t
-  :init
-  (setq quelpa-update-melpa-p nil)
-  (setq quelpa-upgrade-p nil)
-  (setq quelpa-dir (no-littering-expand-var-file-name "quelpa/")
-        quelpa-build-dir (no-littering-expand-var-file-name "quelpa/build/")
-        quelpa-melpa-dir (no-littering-expand-var-file-name "quelpa/melpa/")
-        quelpa-packages-dir (no-littering-expand-var-file-name "quelpa/packages/")
-        quelpa-persistent-cache-file (no-littering-expand-var-file-name "quelpa/cache")))
-
-(use-package quelpa-use-package
-  :ensure t
-  :demand t
-  :config
-  (quelpa-use-package-activate-advice))
-
 ;; Set up use-package for user config
 (setq use-package-always-ensure t)  ; All packages used have to be installed
+
+;; Marry package-vc with use-package.  Quelpa is too heavy.
+(unless (package-installed-p 'vc-use-package)
+  (package-vc-install "https://github.com/slotThe/vc-use-package"))
+(require 'vc-use-package)
 
 (provide 'prelude-package)
