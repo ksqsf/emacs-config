8643b5f22bc102db1d9099755248cdec1a2cbe95
ksqsf <i@ksqsf.moe>
2023-03-20 23:52:55 +0800

    [tex] set up

--- modules/prelude-tex.el
+++ modules/prelude-tex.el
@@ -5,17 +5,17 @@
   :mode (("\\.tex\\'" . LaTeX-mode))
   :commands (TeX-revert-document-buffer TeX-PDF-mode TeX-source-correlate-mode)
   :config
-  (setq-default TeX-engine 'xetex)
+  (setq-default TeX-engine 'default)
   (add-hook 'LaTeX-mode-hook
             #'(lambda ()
-                (setq fill-column 80)
-                (setq TeX-master t)))
+                (setq fill-column 80)))
 
   (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
         TeX-source-correlate-start-server t)
 
   (setq TeX-auto-save t
         TeX-parse-self t)
+  (setq-default TeX-master nil)
 
   (add-hook 'LaTeX-mode-hook #'TeX-source-correlate-mode)
   (add-hook 'LaTeX-mode-hook #'TeX-PDF-mode)
@@ -25,7 +25,7 @@
   (when k|mac
     (setq TeX-source-correlate-mode 'synctex
           TeX-view-program-list
-          '(("Skim"  "/Applications/Skim.app/Contents/SharedSupport/displayline -g -b %n %o %b"))
+          '(("Skim"  "open -a Skim.app %o"))
           TeX-view-program-selection '((output-pdf "Skim"))))
 
   (with-eval-after-load 'eglot
