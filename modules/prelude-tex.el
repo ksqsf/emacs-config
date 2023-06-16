970c95b67604757acb4aed31a1a9d19c6d03a643
ksqsf <i@ksqsf.moe>
2023-06-17 00:40:31 +0800

    [tex] minor refinements

--- modules/prelude-tex.el
+++ modules/prelude-tex.el
@@ -5,7 +5,7 @@
   :mode (("\\.tex\\'" . LaTeX-mode))
   :commands (TeX-revert-document-buffer TeX-PDF-mode TeX-source-correlate-mode)
   :config
-  (setq-default TeX-engine 'default)
+  (setq-default TeX-engine 'luatex)
   (add-hook 'LaTeX-mode-hook
             #'(lambda ()
                 (setq fill-column 80)))
@@ -35,11 +35,19 @@
   :hook ((latex-mode LaTeX-mode) . turn-on-cdlatex)
   :config
   (setq cdlatex-command-alist
-        '(("sum" "Insert \\sum_{}^{}"
-           "\\sum_{?}^{}" cdlatex-position-cursor nil nil t)
-          ("prd" "Insert \\prod_{}^{}"
-           "\\prod_{?}^{}" cdlatex-position-cursor nil nil t)
-          ("tt" "Insert \\texttt{}"
-           "\\texttt{?}" cdlatex-position-cursor nil t t))))
+        '(("sum" "Insert \\sum_{}^{}" "\\sum_{?}^{}" cdlatex-position-cursor nil nil t)
+          ("prd" "Insert \\prod_{}^{}" "\\prod_{?}^{}" cdlatex-position-cursor nil nil t)
+          ("tt" "Insert \\texttt{}" "\\texttt{?}" cdlatex-position-cursor nil t t)
+          ("fr" "Insert \\frac{}{}" "\\frac{?}{?}" cdlatex-position-cursor nil nil t)))
+  (setq cdlatex-env-alist
+        '(("frame" "\\begin{frame}\n\\frametitle{?}\n\n\\end{frame}" nil)))
+  (setq cdlatex-math-symbol-alist
+        '((?0 ("\\varnothing" "\\emptyset"))
+          (?1 ("\\ONE" "\\one"))
+          (?. ("\\cdot" "\\circ"))
+          (?v ("\\vee" "\\bigvee"))
+          (?& ("\\wedge" "\\bigwedge"))
+          (?~ ("\\sim" "\\approx" "\\propto"))))
+  (setq cdlatex-parens-pairs "$[{("))
 
 (provide 'prelude-tex)
