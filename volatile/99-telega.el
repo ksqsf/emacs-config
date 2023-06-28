f09e23b61e7309370e9d8c0ea9ab3d1e60291aa8
ksqsf <i@ksqsf.moe>
2023-06-29 00:21:27 +0800

    disable some volatile

--- volatile/99-telega.el
+++
@@ -1,4 +0,0 @@
-(with-eval-after-load 'telega
-  (add-to-list 'telega-proxies '(:server "127.0.0.1" :port 7890 :enable t :type (:@type "proxyTypeHttp")))
-  (require 'telega-adblock)
-  (telega-adblock-mode 1))
