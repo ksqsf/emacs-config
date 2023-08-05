8448f3afa1d27735490fe3436a8a4d0f1bd77bb3
ksqsf <i@ksqsf.moe>
2023-08-06 02:39:29 +0800

    [lang/agda] defer loading agda file completely

--- modules/prelude-lang-agda.el
+++ modules/prelude-lang-agda.el
@@ -3,11 +3,18 @@
   :group 'prelude
   :type 'string)
 
-;; Load agda-mode only when 'agda-mode' can be found.
-(when-let ((agda-mode-path (or prelude-agda-mode-path
-                               (executable-find "agda-mode"))))
-  (load-file (let ((coding-system-for-read 'utf-8))
-               (shell-command-to-string (concat agda-mode-path " locate")))))
+
+(defun load-agda-mode ()
+  ;; Load agda-mode only when 'agda-mode' can be found.
+  (when-let ((agda-mode-path (or prelude-agda-mode-path
+                                 (executable-find "agda-mode"))))
+    (load-file (let ((coding-system-for-read 'utf-8))
+                 (shell-command-to-string (concat agda-mode-path " locate")))))
+  (if (featurep 'agda2)
+      (agda2-mode)
+    (fundamental-mode)))
+
+(add-to-list 'auto-mode-alist '("\\.agda2?\\'" . load-agda-mode))
 
 (provide 'prelude-lang-agda)
 
