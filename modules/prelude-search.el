10e79b6d2624c07043ecadfd038f10190480e51e
ksqsf <i@ksqsf.moe>
2023-12-08 15:20:51 +0800

    [search] Ignore ".git"

--- modules/prelude-search.el
+++ modules/prelude-search.el
@@ -19,6 +19,7 @@
   :bind ("M-s M-s" . rg-menu))
 
 (with-eval-after-load 'grep
-  (push ".tags" grep-find-ignored-files))
+  (push ".tags" grep-find-ignored-files)
+  (push ".git" grep-find-ignored-files))
 
 (provide 'prelude-search)
