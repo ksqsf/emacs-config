80d1638f9bad3d342bf2f0d54ce55bf2be5de845
ksqsf <i@ksqsf.moe>
2024-02-17 19:50:25 +0800

    [core] Also consider eat in process-kill-query-function.

--- modules/prelude-core.el
+++ modules/prelude-core.el
@@ -418,7 +418,7 @@ Useful for reading Python exception traces."
 ;; Do not query on exit if the term process has no running child process.
 (advice-add 'process-kill-buffer-query-function :before-until
             (lambda (&rest args)
-              (and (derived-mode-p 'vterm-mode 'shell-mode 'term-mode 'comint-mode)
+              (and (derived-mode-p 'vterm-mode 'shell-mode 'term-mode 'comint-mode 'eat-mode)
                    (and (get-buffer-process (current-buffer))
                         (not (process-running-child-p (get-buffer-process (current-buffer))))))))
 
