d861cd1911383a91e69f432dedbc36186f73c3c4
ksqsf <i@ksqsf.moe>
2025-07-04 18:38:39 +0200

    [core] Better default for C-g (from Prot)

--- modules/prelude-core.el
+++ modules/prelude-core.el
@@ -471,4 +471,30 @@ Useful for reading Python exception traces."
 (use-package isearch-mb
   :hook (after-init . isearch-mb-mode))
 
+;; Make keyboard-quit smarter
+(defun +keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+(global-set-key [remap keyboard-quit] #'+keyboard-quit-dwim)
+
 (provide 'prelude-core)
