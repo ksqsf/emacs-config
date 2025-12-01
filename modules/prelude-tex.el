a1b3d1bc2ca263d94cfa8178ae9d73b2bff35835
ksqsf <i@ksqsf.moe>
2025-12-01 11:45:33 +0100

    [tex] Sync minor updates.

--- modules/prelude-tex.el
+++ modules/prelude-tex.el
@@ -5,13 +5,20 @@
   :mode (("\\.tex\\'" . LaTeX-mode))
   :commands (TeX-revert-document-buffer TeX-PDF-mode TeX-source-correlate-mode)
   :config
-  (setq-default TeX-engine 'xetex)
+  (setq-default TeX-engine 'default)
   (add-hook 'LaTeX-mode-hook
             #'(lambda ()
                 (setq fill-column 80)))
 
-  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-        TeX-source-correlate-start-server t)
+
+  (when k|mac
+    (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+          TeX-source-correlate-start-server t))
+  (when k|wsl
+    (setq TeX-view-program-list '(("xdg-open" ("xdg-open '%o'"))))
+    (setq TeX-view-program-selection '((output-pdf "xdg-open"))))
+
+  (setq TeX-command-default "LaTeXMk")
 
   (setq TeX-auto-save t
         TeX-parse-self t)
@@ -47,7 +54,8 @@
         '(("sum" "Insert \\sum_{}^{}" "\\sum_{?}^{}" cdlatex-position-cursor nil nil t)
           ("prd" "Insert \\prod_{}^{}" "\\prod_{?}^{}" cdlatex-position-cursor nil nil t)
           ("tt" "Insert \\texttt{}" "\\texttt{?}" cdlatex-position-cursor nil t t)
-          ("fr" "Insert \\frac{}{}" "\\frac{?}{?}" cdlatex-position-cursor nil nil t)))
+          ("fr" "Insert \\frac{}{}" "\\frac{?}{?}" cdlatex-position-cursor nil nil t)
+          ("rm" "Insert \\mathrm{}" "\\mathrm{?}" cdlatex-position-cursor nil nil t)))
   (setq cdlatex-env-alist
         '(("frame" "\\begin{frame}\n\\frametitle{?}\n\n\\end{frame}" nil)))
   (setq cdlatex-math-symbol-alist
