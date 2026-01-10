d1f9f5e36d7fafe5e3cde2e48771545f86bac903
ksqsf <i@ksqsf.moe>
2026-01-10 19:33:09 +0100

    [lifehacks] Use a dedicated history list for web-search.

--- modules/prelude-lifehacks.el
+++ modules/prelude-lifehacks.el
@@ -1,9 +1,17 @@
 ;;; prelude-lifehacks.el ---                         -*- lexical-binding: t; -*-
+;;;
+;;; A collection of lifehacks.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Web Search
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar web-search-history nil)
 
 (defun web-search ()
   "Search the Internet."
   (interactive)
-  (let* ((input (read-from-minibuffer "Search: "))
+  (let* ((input (read-from-minibuffer "Search: " nil nil nil 'web-search-history))
          (kagi-url (format "https://kagi.com/search?q=%s" (url-encode-url input))))
     (browse-url kagi-url)))
 
