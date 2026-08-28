cab1ff76e68aa05e0204435d4968f485f5604ab2
ksqsf <i@ksqsf.moe>
2026-08-28 18:00:54 +0200

    [term] Add config for Ghostel.

--- modules/prelude-term.el
+++ modules/prelude-term.el
@@ -11,7 +11,7 @@
 ;; Do not query on exit if the term process has no running child process.
 (advice-add 'process-kill-buffer-query-function :before-until
             (lambda (&rest args)
-              (and (derived-mode-p 'vterm-mode 'shell-mode 'term-mode 'comint-mode 'eat-mode)
+              (and (derived-mode-p 'ghostel-mode 'vterm-mode 'shell-mode 'term-mode 'comint-mode 'eat-mode)
                    (and (get-buffer-process (current-buffer))
                         (not (process-running-child-p (get-buffer-process (current-buffer))))))))
 
@@ -93,56 +93,20 @@
 ;; Ghostel
 (use-package ghostel
   :bind
-  (:map ghostel-semi-char-mode-map
-        ("C-q" . ghostel-send-next-key)))
-
-;; VTerm
-(use-package vterm
-  :disabled
-  :commands (vterm)
-  :bind (:map vterm-mode-map
-              ("C-c C-t" . vterm-copy-mode)
-              ("M-!" . shell-command))
-  :custom
-  (vterm-always-compile-module t)
-  :hook (vterm-mode . goto-address-mode)
-  :config
-
-  ;; Integration with desktop-save-mode
-  (defvar vterm-persist-buffer-contents t
-    "When t, desktop-save-mode also saves the buffer contents.")
-  (defun vterm-save-desktop-buffer (dirname)
-    (cons
-     (desktop-file-name default-directory dirname)
-     (if vterm-persist-buffer-contents (buffer-string) "")))
-  (defun vterm-restore-desktop-buffer (_filename buffer-name misc)
-    "MISC is the saved return value of `desktop-save-vterm'."
-    (let ((default-directory (car misc)))
-      (require 'vterm)
-      (with-current-buffer (get-buffer-create buffer-name)
-        (when vterm-persist-buffer-contents
-          (insert (cdr misc))
-          (insert "\n\n"))
-        (vterm-mode))))
-  (add-to-list 'desktop-buffer-mode-handlers '(vterm-mode . vterm-restore-desktop-buffer))
-  (add-hook 'vterm-mode-hook #'(lambda () (setq-local desktop-save-buffer 'vterm-save-desktop-buffer))))
-
-;; (use-package vterm-toggle
-;;   :bind ("M-g v" . vterm-toggle))
+  ( :map ghostel-semi-char-mode-map
+    ("C-q" . ghostel-send-next-key)
+    ("M-o" . nil)
+    ("M-s" . nil)))
 
 ;; Dropdown terminal
 (defun drop-down-term ()
   "Open a drop-down terminal in the same directory as the current file."
   (interactive)
-  (let ((buffer (get-buffer-create "*dd-term*"))
-        win)
-    (with-current-buffer buffer
-      (unless (eq major-mode 'eat-mode)
-        (eat-mode))
-      (unless (and eat-terminal
-                   (eat-term-parameter eat-terminal 'eat--process))
-        (eat-exec buffer (buffer-name) "/usr/bin/env" nil
-                  (list "sh" "-c" shell-file-name))))
+  (let ((ghostel-buffer-name "*dd-term*")
+        buffer win)
+    (ghostel)
+    (previous-buffer)
+    (setq buffer (get-buffer "*dd-term*"))
     (setq win
           (display-buffer-in-side-window
            buffer
@@ -159,44 +123,3 @@
 
 (provide 'prelude-term)
 ;;; prelude-term.el ends here
-
-
-;;; In the past I tried to parametrize my choice of terminal. But
-;;; different terminal packages behave too differently. I think I will
-;;; just stick to vterm or eat.
-
-;; ;; Allow setting preferred terminal emulator.
-;; ;; Think 'pterm' as (1) preferred term, (2) prelude-term, or (3) parametric term.
-;; (defcustom prelude-term 'vterm
-;;   "Preferred terminal emulator. Used by project."
-;;   :group 'prelude
-;;   :type 'symbol)
-
-;; (defun pterm (&rest args)
-;;   (interactive)
-;;   (cond ((eq prelude-term 'eat)
-;;          (funcall 'eat nil args))
-;;         (t
-;;          (funcall 'vterm args))))
-
-;; (defun pterm-other-window (&rest args)
-;;   (interactive)
-;;   (cond ((eq prelude-term 'eat)
-;;          (funcall 'eat-other-window nil args))
-;;         (t
-;;          (funcall 'vterm-other-window args))))
-
-;; (defun pterm-run-in-current-buffer ()
-;;   (interactive)
-;;   (cond ((eq prelude-term 'eat)
-;;          (progn
-;;            (unless (derived-mode-p 'eat-mode)
-;;              (let ((program (or explicit-shell-file-name
-;;                               (getenv "ESHELL")
-;;                               shell-file-name)))
-;;                (eat-mode)
-;;                (eat-exec (current-buffer) (buffer-name) "/usr/bin/env" nil
-;;                          (list "sh" "-c" program))))))
-;;         (t
-;;          (unless (derived-mode-p 'vterm-mode)
-;;            (vterm-mode)))))
